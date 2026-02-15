e936dad38bd54040924690f7611dca66515c50e0
Alexandru Iuga <alex.iuga@gmail.com>
2026-02-15 21:24:52 +0200

    first commit

---
+++ sources/WpfTools.CustomControls/SideBarNavigator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace DustInTheWind.WpfTools.CustomControls;
+
+public class SideBarNavigator : TabControl
+{
+    #region Buttons DependencyProperty
+
+    public static readonly DependencyProperty ButtonsProperty = DependencyProperty.Register(
+        nameof(Buttons),
+        typeof(ObservableCollection<Button>),
+        typeof(SideBarNavigator),
+        new PropertyMetadata(new ObservableCollection<Button>())
+    );
+
+    public ObservableCollection<Button> Buttons
+    {
+        get => (ObservableCollection<Button>)GetValue(ButtonsProperty);
+        set => SetValue(ButtonsProperty, value);
+    }
+
+    #endregion
+
+    static SideBarNavigator()
+    {
+        DefaultStyleKeyProperty.OverrideMetadata(typeof(SideBarNavigator), new FrameworkPropertyMetadata(typeof(SideBarNavigator)));
+    }
+}
