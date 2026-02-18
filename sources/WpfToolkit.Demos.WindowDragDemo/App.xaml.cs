f6795a9b039340ea6fd0d46daf3759e7a98fdddf
Alexandru Iuga <alex.iuga@gmail.com>
2026-02-18 23:44:18 +0200

    added demo app for window drag behavior

---
+++ sources/WpfToolkit.Demos.WindowDragDemo/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace DustInTheWind.WpfToolkit.Demos.WindowDragDemo;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
--- sources/WpfToolkit/WindowDragBehavior.cs
+++ sources/WpfToolkit/WindowDragBehavior.cs
@@ -1,45 +1,44 @@
 using System.Windows;
 using System.Windows.Input;
 
-namespace DustInTheWind.WpfToolkit
+namespace DustInTheWind.WpfToolkit;
+
+public static class WindowDragBehavior
 {
-    public static class WindowDragBehavior
+    public static readonly DependencyProperty EnableDragProperty =
+        DependencyProperty.RegisterAttached(
+            "EnableDrag",
+            typeof(bool),
+            typeof(WindowDragBehavior),
+            new PropertyMetadata(false, OnEnableDragChanged));
+
+    public static bool GetEnableDrag(DependencyObject obj)
     {
-        public static readonly DependencyProperty EnableDragProperty =
-            DependencyProperty.RegisterAttached(
-                "EnableDrag",
-                typeof(bool),
-                typeof(WindowDragBehavior),
-                new PropertyMetadata(false, OnEnableDragChanged));
+        return (bool)obj.GetValue(EnableDragProperty);
+    }
 
-        public static bool GetEnableDrag(DependencyObject obj)
+    public static void SetEnableDrag(DependencyObject obj, bool value)
+    {
+        obj.SetValue(EnableDragProperty, value);
+    }
+
+    private static void OnEnableDragChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+    {
+        if (d is UIElement element)
         {
-            return (bool)obj.GetValue(EnableDragProperty);
+            if ((bool)e.NewValue)
+                element.MouseLeftButtonDown += Element_MouseLeftButtonDown;
+            else
+                element.MouseLeftButtonDown -= Element_MouseLeftButtonDown;
         }
+    }
 
-        public static void SetEnableDrag(DependencyObject obj, bool value)
+    private static void Element_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (sender is DependencyObject dependencyObject)
         {
-            obj.SetValue(EnableDragProperty, value);
-        }
-
-        private static void OnEnableDragChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (d is UIElement element)
-            {
-                if ((bool)e.NewValue)
-                    element.MouseLeftButtonDown += Element_MouseLeftButtonDown;
-                else
-                    element.MouseLeftButtonDown -= Element_MouseLeftButtonDown;
-            }
-        }
-
-        private static void Element_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (sender is DependencyObject dependencyObject)
-            {
-                Window window = Window.GetWindow(dependencyObject);
-                window?.DragMove();
-            }
+            Window window = Window.GetWindow(dependencyObject);
+            window?.DragMove();
         }
     }
 }
